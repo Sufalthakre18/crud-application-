5588d1aba85ddc0b629cf1fac77d1d4e91359c58
Sufal Thakre <sufalthakre1@gmail.com>
2026-01-08 10:05:37 +0530

    Create Database Connection and Model

---
+++ notes-app/lib/mongodb.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import Error from "next/error";
+
+const MONGODB_URI=process.env.MONGODB_URI
+if(!MONGODB_URI){
+    throw new Error("MONGODB_URI environment variable not correct")
+}
+
+async function dbConnect() {
+    if(mongoose.connections[0].readyState) return;
+    try {
+        await mongoose.connect(MONGODB_URI)
+        console.log("connected to mongodb");
+    } catch (error) {
+        console.error(error);
+        throw new Error("database connection failed")
+    }
+}
+
+export default dbConnect;
---
+++ notes-app/models/Note.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const noteSchema= new mongoose.Schema({
+    title:{type:String,required:true},
+    cotent:{type:String,required:true},
+    createdAt:{type:Date,default:Date.now}
+})
+
+const Note= mongoose.model.Note || mongoose.model("Note",noteSchema)
+
+export default Note;
