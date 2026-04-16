d4987f0079697a39630bdb6a859f7eaa8ce10c72
Sufal Thakre <sufalthakre1@gmail.com>
2026-04-16 15:02:58 +0530

    Add functions to get and save notes in localStorage

--- notes-app/utils/localStorage.js
+++ notes-app/utils/localStorage.js
@@ -1,1 +1,9 @@
+export const getNotes = () => {
+    if (typeof window === "undefined") return [];
+    const notes = localStorage.getItem("notes");
+    return notes ? JSON.parse(notes) : [];
+};
 
+export const saveNotes = (notes) => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+};
